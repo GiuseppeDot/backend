cb23d686342b286555662bdc51996511197859fa
GiuseppeDot <giuseppegianni190@gmail.com>
2025-01-08 17:58:52 +0100

    Initial commit

---
+++ project2/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner mioscanner = new Scanner(System.in);
+        System.out.println("Digita il tuo nome: ");
+        String nomeUtente = mioscanner.nextLine();
+        System.out.println("Digita il tuo cognome: ");
+        String CognomeUtente = mioscanner.nextLine();
+
+
+    }
+}
---
+++ project2/src/Switch/StampaLettura.java
@@ -0,0 +1,30 @@
+package Switch;
+
+import java.util.Scanner;
+
+public class StampaLettura {
+    public static void stampaLettura(input) {
+        switch (input){
+                case 0: {
+                    System.out.println("zero");
+                    break; }
+                case 1: {
+                    System.out.println("uno");
+                    break; }
+                case 2: {
+                    System.out.println("due");
+                    break; }
+                case 3: {
+                    System.out.println("tre");
+                    break; }
+                default: {
+                System.out.println("non è cpìompreso tra 0 e 3");
+                }
+            }
+    }
+    public static void main (String[] args) {
+        Scanner myScanner = new Scanner (System.in);
+        System.out.println("Inserisci il numero: ");
+        
+    }
+}
