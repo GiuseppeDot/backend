526598a93e11644eaaf3fcc95e4460e2f73b6dea
GiuseppeDot <giuseppegianni190@gmail.com>
2025-01-10 15:05:23 +0100

    commit

---
+++ OOP/src/forme/Forma.java
@@ -0,0 +1,4 @@
+package forme;
+
+public class Forma {
+}
---
+++ OOP/src/forme/Triangolo.java
@@ -0,0 +1,14 @@
+package forme;
+
+public class Triangolo extends Forma{
+
+    private double base;
+    private double altezza;
+
+    public Triangolo (double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+
+}
